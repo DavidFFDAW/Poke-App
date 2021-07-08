6814bf62752543fbb3cd4e81255e16492d60dbb3
DavidFFDAW <davidferflo2@gmail.com>
2021-07-09 00:20:31 +0200

    refactor(App.js): added new route for pokemon searching

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,7 @@ import PokeHeader from './components/PokeHeader/PokeHeader';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './pages/Home';
 import PokemonDetailPage from './pages/PokemonDetailPage';
+import PokemonSearchPage from './pages/PokemonSearchPage';
 import './App.css';
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
         </Route>
         <Route path="/pokemon/:name" exact>
           <PokemonDetailPage/>
+        </Route>
+        <Route path="/pokemon/search/:name" exact>
+          <PokemonSearchPage/>
         </Route>
       </Switch>
     </Router>
