4ae11f9aab08502667a6712e1b0623d479213c28
DavidFFDAW <davidferflo2@gmail.com>
2021-07-09 00:15:36 +0200

    feat(i18n): starting the internationalization of abilities

---
+++ web/src/i18n/abilities.js
@@ -0,0 +1,13 @@
+export default {
+    'pressure': 'presión',
+    'inner-focus': 'foco interno',
+    'drizzle': 'llovizna',
+    'swarm': 'enjambre',
+    'weak-armor': 'armadura fragil',    
+    'aftermath': 'resquicio',    
+    'keen-eye': 'vista lince',    
+    'big-pecks': 'sacapecho',    
+    'hydration': 'hidratación',    
+    'stench': 'hedor',    
+    'stench': 'hedor',    
+}
