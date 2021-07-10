fbb2502d61d29ec954a6ee65c025cbdb8de3969e
DavidFFDAW <davidferflo2@gmail.com>
2021-07-10 02:25:40 +0200

    feat(App): added some more general and shared styles and changes in App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PokeHeader from './components/PokeHeader/PokeHeader';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { getAllPokemons } from './services/PokeAPI.service';
+
 import HomePage from './pages/Home';
 import PokemonDetailPage from './pages/PokemonDetailPage';
 import PokemonSearchPage from './pages/PokemonSearchPage';
 import './App.css';
 
-function App() {
+function App() {  
+
   return (
     <Router>
       <PokeHeader></PokeHeader>
