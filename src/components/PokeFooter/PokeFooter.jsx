016af0c1fcabe3d85f0b9e275f848886708e6749
DavidFFDAW <davidferflo2@gmail.com>
2021-07-12 19:04:07 +0200

    feat(footer): more changes on the footer

--- src/components/PokeFooter/PokeFooter.jsx
+++ src/components/PokeFooter/PokeFooter.jsx
@@ -3,13 +3,13 @@ import { config } from "../../constants/config"
 export default function Footer(){
     return (
         <div className="flex center footer">
-            <div>
+            <div className="all-margins">
                 <a href={ config.repo }>PokeInfoApp</a> 
                 <span>@Copyright</span> 
                 <span>{ config.year }</span> 
                 <a href={config.author.web}>{ config.author.name }</a> 
                 <span>v{config.version}</span>
-                <span>fase: {config.phase}</span>
+                <span>fase: {config.phase}</span>            
             </div>
         </div>
     );
