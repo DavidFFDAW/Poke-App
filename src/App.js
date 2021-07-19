01616e004a06742e811bd3dc93f8b99c4b4e51a0
DavidFFDAW <davidferflo2@gmail.com>
2021-07-19 22:55:57 +0200

    feat(app): added new route for info of a pokemon move

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PokeHeader from './components/PokeHeader/PokeHeader';
 import PokeFooter from './components/PokeFooter/PokeFooter';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import { getAllPokemons } from './services/PokeAPI.service';
 
 import HomePage from './pages/Home';
 import PokemonDetailPage from './pages/PokemonDetailPage';
 import PokemonSearchPage from './pages/PokemonSearchPage';
+import PokeMoveInfoPage from './pages/PokeMoveInfoPage';
 import './App.css';
 
 function App() {  
@@ -23,6 +23,9 @@ function App() {
         </Route>
         <Route path="/pokemon/search/:name" exact>
           <PokemonSearchPage/>
+        </Route>
+        <Route path="/pokemon/move/info/:move" exact>
+          <PokeMoveInfoPage/>
         </Route>
       </Switch>
       <PokeFooter></PokeFooter>
