bc5a0086064ba0c57f427614cadae88775e6d929
DavidFFDAW <davidferflo2@gmail.com>
2021-07-19 22:38:47 +0200

    abstracted RoundedBox out in a component to be reusable

---
+++ src/components/RoundedBox/RoundedBox.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function RoundedBox ({ title, children }){
+    return (
+        <div className="rounded-box">
+            <h3 className="title box-title"><strong>{ title } </strong></h3>
+            <div className="body flex-strt flxwrap">
+                { children }
+            </div>
+        </div>
+    );
+}
+export function RoundedBoxPad30 ({ title, children }){
+    return (
+        <div className="rounded-box">
+            <h3 className="title box-title"><strong>{ title } </strong></h3>
+            <div className="flex center pad-30">
+                { children }
+            </div>
+        </div>
+    );
+}
+
+export function SimpleRoundBox({ title, children }){
+    return (
+        <div className="rounded-box first">
+            <h3 className="title box-title"><strong>{ title } </strong></h3>
+            <div className="body">
+                { children }
+            </div>
+        </div>
+
+    );
+}
