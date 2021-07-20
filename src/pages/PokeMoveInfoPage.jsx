a848dd333be20f301adfdbe2f051d96024168075
DavidFFDAW <davidferflo2@gmail.com>
2021-07-20 22:36:04 +0200

    feat(pokeMovePage): added translation of move name from move page

--- src/pages/PokeMoveInfoPage.jsx
+++ src/pages/PokeMoveInfoPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useCustomTranslate from '../hooks/useTranslate';
 import ScrollView from '../components/ScrollView/ScrollView';
 import MoveDetails from '../components/MoveDetails/MoveDetails';
 
 export default function PokeMoveInfoPage() {
 
+    const { translateMove } = useCustomTranslate();
     const { move } = useParams();
 
     return (
         <>
-            <div className="flex center details-title-name bg-top">{ move }</div>
+            <div className="flex center details-title-name bg-top">{ translateMove(move) }</div>
             <ScrollView>
                 <MoveDetails name={ move } />
             </ScrollView>
