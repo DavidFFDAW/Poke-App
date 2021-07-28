bb45bfd5529df349fc6b7ec81bcf3fd8bb9d3f14
DavidFFDAW <davidferflo2@gmail.com>
2021-07-28 22:47:33 +0200

    refactor(TypeTag): changed component to add default value in case some type is not include

--- src/components/TypeTag/TypeTag.jsx
+++ src/components/TypeTag/TypeTag.jsx
@@ -5,12 +5,14 @@ import { types } from '../../constants/types.data';
 export function TypeTag({ type, css }){
 
     const { translateType } = useTranslate();
+    const bgColor = types[type] ? types[type].bg : '#eee';
+    const fontColor = types[type] ? types[type].font : '#222';
 
     return (
         <span 
             className={ css || 'poke-type-tag' } style={{
-                background: types[type].bg,
-                color: types[type].font,
+                background: bgColor,
+                color: fontColor,
         }}>
             { translateType(type) }
         </span>
