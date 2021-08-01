329d27095f0439051451cc062c07df03f17b1157
DavidFFDAW <davidferflo2@gmail.com>
2021-08-01 18:48:59 +0200

    feat(useTranslate): added methods to get the languague and the original move name based on the lang

--- src/hooks/useTranslate.js
+++ src/hooks/useTranslate.js
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 export default function useTranslate(){
 
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
 
     const translateMove = useCallback(toTranslate => {        
         return t('moves')[toTranslate.toLowerCase()] || toTranslate;
@@ -12,9 +12,17 @@ export default function useTranslate(){
     const translateType = useCallback(toTranslate => {
         return t('pokemon-types')[toTranslate.toLowerCase()] || toTranslate;
     },[ t ]);
+
     const translateEggType = useCallback(toTranslate => {
         return t('poke-egg-group')[toTranslate.toLowerCase()] || toTranslate;
     },[ t ]);
 
-    return { translateMove, translateType, translateEggType }
+    const getLanguage = useCallback(_ => i18n.language,[ i18n ])
+
+    const getMoveFromTranslation = useCallback(introducedMove => {
+        const moves = t('moves');
+        return Object.keys(moves).find(move => moves[move].toLowerCase() === introducedMove.toLowerCase()) || null;
+    },[ t ]);
+
+    return { translateMove, translateType, translateEggType, getLanguage, getMoveFromTranslation }
 }
