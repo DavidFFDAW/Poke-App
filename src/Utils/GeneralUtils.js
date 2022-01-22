f1513d8d432cd54e387a001372bb09190657a142
DavidFFDAW <davidferflo2@gmail.com>
2022-01-22 19:30:51 +0100

    feat(Utils): added new Utils

---
+++ src/Utils/GeneralUtils.js
@@ -0,0 +1,42 @@
+import { types } from "../constants/types.data";
+
+export const capitalizeName = (name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLoweCase();
+}
+
+export const getTypeColor = (type) => {
+    const typeBg = types[type];
+
+    if (!typeBg) {
+        return null;
+    }
+
+    return {
+        bg: '#' + typeBg.bg.split('#')[1].replace(',',''),
+        border: typeBg.bg.split(',')[1].replace(')',''),
+    }
+};
+
+export const parseChartStatistics = (stats, pokeName, color = { bg: '#dea9b1', border: 'dea9c6' }) => {
+    const parsedStats = stats.map((it) => ({
+        name: it.stat.name,
+        base_stat: it.base_stat,
+        effort: it.effort
+    }));
+
+    const statNumbers = Object.values(parsedStats).map(stat => stat.base_stat);
+
+    var data = [{
+        label: `${pokeName} stats`,
+        data: statNumbers,
+        backgroundColor: color.bg,                  
+        borderColor: color.border,
+    }];
+
+    return { 
+        labels: Object.values(parsedStats).map(stat => 
+            stat.name.toUpperCase()
+        ),
+        datasets: data,
+    }
+}
