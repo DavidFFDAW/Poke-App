cc6c7eeebc004180db406fb3f8e8ec8a015d2bb1
DavidFFDAW <davidferflo2@gmail.com>
2022-01-22 19:33:38 +0100

    build(package): added test and new dependencies in package.json

--- src/test.js
+++ src/test.js
@@ -1,37 +1,41 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-const number = 1;
-const url = `https://pokeapi.co/api/v2/evolution-chain/${ number }`;
+// const number = 1;
+// const url = `https://pokeapi.co/api/v2/evolution-chain/${ number }`;
 
 
-const addToObject = (object,key,element) => {
-    object[key] = element;
-    return object;
-}
+// const addToObject = (object,key,element) => {
+//     object[key] = element;
+//     return object;
+// }
 
-const getEvolutions = async () => {
-    let finalArray = [];
-    let evoData = null;
+// const getEvolutions = async () => {
+//     let finalArray = [];
+//     let evoData = null;
 
-    const response = await(await fetch(url)).json();
-    console.log(response.chain);
-    evoData = response.chain;
+//     const response = await(await fetch(url)).json();
+//     console.log(response.chain);
+//     evoData = response.chain;
     
-    do{
-        let evDetail = evoData.evolution_details[0];
+//     do{
+//         let evDetail = evoData.evolution_details[0];
 
-        finalArray.push({
-            name: evoData.species.name,
-            min_level: !evDetail ? 1 : evDetail.min_level,
-            trigger_name: !evDetail ? null : evDetail.trigger_name,
-            item: !evDetail ? null : evDetail.item,
-        });
-        evoData = evoData.evolves_to[0];
+//         finalArray.push({
+//             name: evoData.species.name,
+//             min_level: !evDetail ? 1 : evDetail.min_level,
+//             trigger_name: !evDetail ? null : evDetail.trigger_name,
+//             item: !evDetail ? null : evDetail.item,
+//         });
+//         evoData = evoData.evolves_to[0];
 
-    } while (!!evoData && evoData.hasOwnProperty('evolves_to'));
+//     } while (!!evoData && evoData.hasOwnProperty('evolves_to'));
 
-    return finalArray;
-}
+//     return finalArray;
+// }
 
 
-getEvolutions().then(console.log);
+// getEvolutions().then(console.log);
+
+const gr = 'linear-gradient( #FAE078,#F8D030 )';
+console.log('#' + gr.split('#')[1].replace(',',''));
+console.log(gr.split(',')[1].replace(')',''));
